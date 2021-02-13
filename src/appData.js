00956bb7a0a104c0bc163c3a5b04e6f3c3b47971
NagarjunM-dev <nagarjunm2012@gmail.com>
2021-02-13 18:35:36 +0530

    address to UI task

---
+++ src/appData.js
@@ -0,0 +1,14 @@
+export const rowData = [
+    {
+		product_name: ProductA,
+		product_description: Lorumipsum,
+		is_active: true,
+		price: 44,
+		offer_price: 34,
+		offer_start_at: new Date(2020-02-030000),
+		offer_end_at: new Date(2020-02-030000),
+		created_at: new Date(2020-02-030000),
+		updated_at: new Date(2020-02-030000)
+          }
+
+]
